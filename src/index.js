0c12669241cdd24080bd46c1c6de9712f767970a
Felipe almeida <felipepereira937@gmail.com>
2018-09-02 00:36:36 -0300

    configurando react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('My Minimal React Webpack Babel Setup');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Setup Template for React App';
+
+ReactDOM.render(
+  title,
+  document.getElementById('app')
+);
+
+module.hot.accept();
